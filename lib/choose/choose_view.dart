166f1e3a07f572ffa0c1d6a6cd6a0d0ef4557329
emreozturk <emre_oztrk_99@hotmail.com>
2021-11-02 14:52:23 +0300

    Choose View Updated

--- lib/choose/choose_view.dart
+++ lib/choose/choose_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:horoscope_guide/routes/app_pages.dart';
 
 class ChooseView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('LEO'),
@@ -12,36 +15,119 @@ class ChooseView extends StatelessWidget {
         child: Column(
           children: [
             Image.asset('assets/horoscopes/leo.jpg'),
+            SizedBox(height: deviceSize.height / 50),
             const Text('LEO'),
+            SizedBox(height: deviceSize.height / 50),
             const Text('24 July - 23 August'),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                height: 100,
-                width: 100,
-                child: Row(
-                  children: const [
-                    Icon(Icons.female),
-                    Text('FEMALE'),
-                  ],
-                ),
+            SizedBox(height: deviceSize.height / 50),
+            buttonView(
+              deviceSize,
+              'GENERAL INFORMATION',
+              Icons.male,
+              deviceSize.height / 20,
+              deviceSize.width / 1.75,
+            ),
+            SizedBox(height: deviceSize.height / 50),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  buttonView(
+                    deviceSize,
+                    'MALE     ',
+                    Icons.male,
+                    deviceSize.height / 20,
+                    deviceSize.width / 3,
+                  ),
+                  buttonView(
+                    deviceSize,
+                    'FEMALE   ',
+                    Icons.female,
+                    deviceSize.height / 20,
+                    deviceSize.width / 3,
+                  ),
+                ],
               ),
             ),
-            /*ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(100, 50),
-                primary: const Color(0xFF01122C),
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
+            SizedBox(height: deviceSize.height / 50),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  buttonView(
+                    deviceSize,
+                    'POSITIVE ',
+                    Icons.add,
+                    deviceSize.height / 20,
+                    deviceSize.width / 3,
+                  ),
+                  buttonView(
+                    deviceSize,
+                    'NEGATIVE ',
+                    Icons.clear,
+                    deviceSize.height / 20,
+                    deviceSize.width / 3,
+                  ),
+                ],
               ),
-              child: const Text('GENERAL INFORMATION'),
-              onPressed: () {},
-            ),*/
+            ),
+            SizedBox(height: deviceSize.height / 50),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  buttonView(
+                    deviceSize,
+                    'RISING   ',
+                    Icons.call_made,
+                    deviceSize.height / 20,
+                    deviceSize.width / 3,
+                  ),
+                  buttonView(
+                    deviceSize,
+                    'LOVE LIFE',
+                    Icons.local_airport_rounded,
+                    deviceSize.height / 20,
+                    deviceSize.width / 3,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+InkWell buttonView(Size deviceSize, String buttonName, IconData icon,
+    double height, double width) {
+  return InkWell(
+    onTap: () => Get.toNamed(Routes.detailView),
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      height: height,
+      width: width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(icon),
+          SizedBox(width: deviceSize.width / 50),
+          Text(buttonName),
+        ],
+      ),
+    ),
+  );
 }
